fa1348a7c8c738337ecff765341486eac8f1b900
Thai Nguyen <thai.nguyen18@gmail.com>
2018-04-03 22:30:57 -0400

    add Gershgorin disc

---
+++ GershgorinDisc.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+
+
+def create_gershgorin_discs(mat):
+    # TODO: Check if matrix is square
+
+    n = len(mat)
+
+    x = np.zeros(n)
+    y = np.zeros(n)
+    r = np.zeros(n)
+    for i in range(n):
+        x[i] = np.real(mat[(i, i)])
+        y[i] = np.imag(mat[(i, i)])
+        r[i] = np.sum(np.abs(mat[i, :])) - np.abs(mat[i, i])
+
+    return x, y, r
+
+
+def plot_gershgorin_discs(x, y, r):
+    # TODO: Check if x, y, r vectors are valid (all same length, contains complex numbers)
+
+    # Draw discs
+    n = x.size
+    patches = []
+    for i in range(n):
+        circle = Circle((x[i], y[i]), r[i])
+        patches.append(circle)
+
+    # Plot
+    fig, ax = plt.subplots()
+    p = PatchCollection(patches, alpha=0.2)
+    ax.add_collection(p)
+    plt.axis('equal')
+    plt.plot(x, y, marker='x', linestyle='none')
+
+    plt.xlabel('Real')
+    plt.ylabel('Imaginary')
+    plt.grid()
+    plt.show()
+
+    return None
+
+
+if __name__ == '__main__':
+    # Test functions
+    A = np.matrix('10, -1, 0, 1; 0.2, 8 0.2, 0.2; 1, 1, 2, 1; -1, -1, -1, -11')
+    x, y, r = create_gershgorin_discs(A)
+    plot_gershgorin_discs(x, y, r)
