d945e4741cb0d2c877fa284061b5a8821b68659a
Thai Nguyen <thai.nguyen18@gmail.com>
2018-04-06 20:59:26 -0400

    implemented create_companion_matrix function, fixed some unit tests.

--- CompanionMatrix.py
+++ CompanionMatrix.py
@@ -1,4 +1,11 @@
+import numpy as np
 
 
-def create_companion_matrix(coefficeints):
-    return None
+def create_companion_matrix(coefficients):
+    # TODO: Check if input is valid and check if leading coefficient is zero
+
+    n = coefficients.size-1
+    c = np.flip(coefficients, 0)
+    mat = np.eye(n, k=-1)
+    mat[:, n-1] = -c[:n]
+    return mat
--- tests/test_create_companion_matrix.py
+++ tests/test_create_companion_matrix.py
@@ -1,53 +1,49 @@
 from unittest import TestCase
+import numpy as np
 
 
 class TestCreateCompanionMatrix(TestCase):
     def test_odd_leading_coefficient(self):
         # Testing case where the given polynomial is
         # x^3 + 3x^2 + 4x + 5
-        import numpy as np
         from CompanionMatrix import create_companion_matrix
         coeffs = np.array((1, 3, 4, 5))
         test_mat = create_companion_matrix(coeffs)
         true_mat = np.matrix('0, 0, -5; 1, 0, -4; 0, 1, -3')
-        self.assertEqual(test_mat, true_mat)
+        np.testing.assert_array_equal(test_mat, true_mat)
 
     def test_even_leading_coefficient(self):
         # Testing case where the given polynomial is
         # x^4 + 5x^3 + 4x^2 + 2x + 1
-        import numpy as np
         from CompanionMatrix import create_companion_matrix
         coeffs = np.array((1, 5, 4, 2, 1))
         test_mat = create_companion_matrix(coeffs)
         true_mat = np.matrix('0, 0, 0, -1; 1, 0, 0, -2; 0, 1, 0, -4; 0, 0, 1, -5')
-        self.assertEqual(test_mat, true_mat)
+        np.testing.assert_array_equal(test_mat, true_mat)
 
     def test_one_zero_coefficient(self):
         # Testing case where the given polynomial is
         # x^2 + 0x + 1
-        import numpy as np
         from CompanionMatrix import create_companion_matrix
         coeffs = np.array((1, 0, 1))
         test_mat = create_companion_matrix(coeffs)
         true_mat = np.matrix('0, -1; 1, 0')
-        self.assertEqual(test_mat, true_mat)
+        np.testing.assert_array_equal(test_mat, true_mat)
 
     def test_leading_coefficient_zero(self):
         # Testing case where the given polynomial is
         # 0x^2 + x + 1
-        import numpy as np
         from CompanionMatrix import create_companion_matrix
         coeffs = np.array((0, 1, 1))
         test_mat = create_companion_matrix(coeffs)
         true_mat = np.matrix('-1')
-        self.assertEqual(test_mat, true_mat)
+        np.testing.assert_array_equal(test_mat, true_mat)
 
     def test_zero_constant_term(self):
         # Testing case where the given polynomial is
         # x^2 + x + 0
-        import numpy as np
         from CompanionMatrix import create_companion_matrix
-        coeffs = np.array((0, 1, 1))
+        coeffs = np.array((1, 1, 0))
         test_mat = create_companion_matrix(coeffs)
         true_mat = np.matrix('0, 0; 1, -1')
-        self.assertEqual(test_mat, true_mat)
+        np.testing.assert_array_equal(test_mat, true_mat)
