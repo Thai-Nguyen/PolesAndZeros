1d7fcc8eaaaa4537fb79290d64d6d48549b89e59
Thai Nguyen <thai.nguyen18@gmail.com>
2018-04-06 21:10:35 -0400

    -m added some comments

--- CompanionMatrix.py
+++ CompanionMatrix.py
@@ -4,8 +4,10 @@ import numpy as np
 def create_companion_matrix(coefficients):
     # TODO: Check if input is valid and check if leading coefficient is zero
 
-    n = coefficients.size-1
-    c = np.flip(coefficients, 0)
+    n = coefficients.size-1  # determine size of companion matrix
+    c = np.flip(coefficients, 0)  # reorder so leading coefficient is last
+
+    # create companion matrix
     mat = np.eye(n, k=-1)
     mat[:, n-1] = -c[:n]
     return mat
