62dd004a439462408ca177c65fa604ba0cd8c1c9
Thai Nguyen <thai.nguyen18@gmail.com>
2018-04-09 21:35:52 -0400

    implemented schur transform

--- tests/test_create_gershgorin_discs.py
+++ tests/test_create_gershgorin_discs.py
@@ -1,15 +1,27 @@
 from unittest import TestCase
 
 
-class TestCreate_gershgorin_discs(TestCase):
+class TestCreateGershgorinDiscs(TestCase):
     def test_vector_input(self):
         import numpy as np
         from GershgorinDisc import create_gershgorin_discs
-        vec = np.matrix('1, 1, 1')
+        vec = np.array([[1, 1, 1]])
         self.assertFalse(create_gershgorin_discs(vec))
 
     def test_non_square_matrix_input(self):
         import numpy as np
         from GershgorinDisc import create_gershgorin_discs
-        A = np.matrix('1, 1, 1; 1, 1, 1')
-        create_gershgorin_discs(A)
+        mat = np.array([[1, 1, 1], [1, 1, 1]])
+        self.assertFalse(create_gershgorin_discs(mat))
+
+    def test_empty_matrix(self):
+        import numpy as np
+        from GershgorinDisc import create_gershgorin_discs
+        mat = np.array([[]])
+        self.assertFalse(create_gershgorin_discs(mat))
+
+    def test_scalar_input(self):
+        import numpy as np
+        from GershgorinDisc import create_gershgorin_discs
+        mat = np.array([[1]])
+        self.assertFalse(create_gershgorin_discs(mat))
